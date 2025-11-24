17cc4613f129b19f104af82ac8380187ffd82968
AngelMaroto <angel.margar@educa.jcyl.es>
2025-11-24 12:42:44 +0100

    cambios

--- addons/manage_angel/models/models.py
+++ addons/manage_angel/models/models.py
@@ -1,19 +1,52 @@
-# -*- coding: utf-8 -*-
+from odoo import models, fields
 
-# from odoo import models, fields, api
+class Project(models.Model):
+    _name = 'manage.project'
+    _description = 'Project'
 
+    name = fields.Char(string='Name', required=True)
+    description = fields.Char(string='Description')
+    history_ids = fields.One2many('manage.history', 'project_id', string='User Stories')
+    sprint_ids = fields.One2many('manage.sprint', 'project_id', string='Sprints')
 
-# class manage_angel(models.Model):
-#     _name = 'manage_angel.manage_angel'
-#     _description = 'manage_angel.manage_angel'
+class Sprint(models.Model):
+    _name = 'manage.sprint'
+    _description = 'Sprint'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    name = fields.Char(string='Name', required=True)
+    description = fields.Char(string='Description')
+    startdate = fields.Datetime(string='Start Date')
+    enddate = fields.Datetime(string='End Date')
+    project_id = fields.Many2one('manage.project', string='Project')
+    task_ids = fields.One2many('manage.task', 'sprint_id', string='Tasks')
 
+class History(models.Model):
+    _name = 'manage.history'
+    _description = 'User Story (History)'
+
+    name = fields.Char(string='Name', required=True)
+    description = fields.Char(string='Description')
+    project_id = fields.Many2one('manage.project', string='Project')
+    task_ids = fields.One2many('manage.task', 'history_id', string='Tasks')
+
+class Task(models.Model):
+    _name = 'manage.task'
+    _description = 'Task'
+
+    name = fields.Char(string='Name', required=True)
+    description = fields.Char(string='Description')
+    startdate = fields.Datetime(string='Start Date')
+    enddate = fields.Datetime(string='End Date')
+    ispaused = fields.Boolean(string='Is Paused')
+    history_id = fields.Many2one('manage.history', string='User Story')
+    sprint_id = fields.Many2one('manage.sprint', string='Sprint')
+    technology_ids = fields.Many2many('manage.technology', string='Technologies')
+
+class Technology(models.Model):
+    _name = 'manage.technology'
+    _description = 'Technology'
+
+    name = fields.Char(string='Name', required=True)
+    description = fields.Char(string='Description')
+    photo = fields.Image(string='Photo')
+    task_ids = fields.Many2many('manage.task', string='Tasks')
