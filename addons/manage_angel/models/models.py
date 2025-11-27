40bd9ba770012060e8f8eb277f61cbc9a9c26942
AngelMaroto <angel.margar@educa.jcyl.es>
2025-11-27 11:42:39 +0100

    cambios

--- addons/manage_angel/models/models.py
+++ addons/manage_angel/models/models.py
@@ -1,63 +1,111 @@
 from odoo import models, fields, api
-import datetime
+from datetime import timedelta
 
 class Project(models.Model):
     _name = 'manage.project'
     _description = 'Project'
 
-    name = fields.Char(string='Name', required=True)
-    description = fields.Char(string='Description')
-    history_ids = fields.One2many('manage.history', 'project_id', string='User Stories')
-    sprint_ids = fields.One2many('manage.sprint', 'project_id', string='Sprints')
+    name = fields.Char(required=True)
+    description = fields.Char()
+    history_ids = fields.One2many('manage.history', 'project_id')
+    sprint_ids = fields.One2many('manage.sprint', 'project_id')
+
+    code = fields.Char(compute="_compute_code", store=True)
+
+    @api.depends('id')
+    def _compute_code(self):
+        for project in self:
+            project.code = f"PROY_{project.id or 0}"
+
 
 class Sprint(models.Model):
     _name = 'manage.sprint'
     _description = 'Sprint'
 
-    name = fields.Char(string='Name', required=True)
-    description = fields.Char(string='Description')
+    name = fields.Char(required=True)
+    description = fields.Char()
     duration = fields.Integer()
-    startdate = fields.Datetime(string='Start Date')
-    enddate = fields.Datetime(compute="_get_end_date", store=True, string='End Date')
-    project_id = fields.Many2one('manage.project', string='Project')
-    task_ids = fields.One2many('manage.task', 'sprint_id', string='Tasks')
+    startdate = fields.Datetime()
+    enddate = fields.Datetime(compute="_compute_enddate", store=True)
+    project_id = fields.Many2one('manage.project')
+    task_ids = fields.One2many('manage.task', 'sprint_id')
 
-    @api.depends('startdate','duration')
-    def _get_end_date(self):
-        for Sprint in self:
-            #try:
-                if isinstance(Sprint.startdate, datetime.datetime) and Sprint.duration > 0:
-                     Sprint.enddate = Sprint.startdate + datetime.timedelta(days=Sprint.duration)
-                else:
-                     Sprint.enddate = Sprint.startdate
+    @api.depends('startdate', 'duration')
+    def _compute_enddate(self):
+        for sprint in self:
+            if sprint.startdate and sprint.duration:
+                sprint.enddate = sprint.startdate + timedelta(days=sprint.duration)
+            else:
+                sprint.enddate = False
+
 
 class History(models.Model):
     _name = 'manage.history'
-    _description = 'User Story (History)'
+    _description = 'User Story'
 
-    name = fields.Char(string='Name', required=True)
-    description = fields.Char(string='Description')
-    project_id = fields.Many2one('manage.project', string='Project')
-    task_ids = fields.One2many('manage.task', 'history_id', string='Tasks')
+    name = fields.Char(required=True)
+    description = fields.Char()
+    project_id = fields.Many2one('manage.project')
+    task_ids = fields.One2many('manage.task', 'history_id')
+
+    used_technologies = fields.Many2many(
+        "manage.technology",
+        compute="_compute_used_technologies",
+        store=True
+    )
+
+    @api.depends('task_ids', 'task_ids.technology_ids')
+    def _compute_used_technologies(self):
+        for history in self:
+            technologies = self.env['manage.technology']
+            for task in history.task_ids:
+                technologies |= task.technology_ids
+            history.used_technologies = technologies
+
 
 class Task(models.Model):
     _name = 'manage.task'
     _description = 'Task'
 
-    name = fields.Char(string='Name', required=True)
-    description = fields.Char(string='Description')
-    startdate = fields.Datetime(string='Start Date')
-    enddate = fields.Datetime(string='End Date')
-    ispaused = fields.Boolean(string='Is Paused')
-    history_id = fields.Many2one('manage.history', string='User Story')
-    sprint_id = fields.Many2one('manage.sprint', string='Sprint')
-    technology_ids = fields.Many2many('manage.technology', string='Technologies')
+    name = fields.Char(required=True)
+    description = fields.Char()
+    startdate = fields.Datetime()
+    enddate = fields.Datetime()
+    ispaused = fields.Boolean()
+    history_id = fields.Many2one('manage.history')
+    sprint_id = fields.Many2one('manage.sprint', compute='_compute_sprint', store=True)
+    technology_ids = fields.Many2many('manage.technology')
+    # campo relacionado que refleja el proyecto de la historia
+    project_id = fields.Many2one('manage.project', related='history_id.project_id', readonly=True, store=True)
+
+    code = fields.Char(compute="_compute_code", store=True)
+
+    @api.depends('id')
+    def _compute_code(self):
+        for task in self:
+            # Código de tarea, cámbialo si prefieres otro formato
+            task.code = f"TASK_{task.id or 0}"
+
+    @api.depends('history_id', 'history_id.project_id', 'history_id.project_id.sprint_ids.enddate')
+    def _compute_sprint(self):
+        for task in self:
+            if not task.history_id or not task.history_id.project_id:
+                task.sprint_id = False
+                continue
+
+            sprint = self.env['manage.sprint'].search([
+                ('project_id', '=', task.history_id.project_id.id),
+                ('enddate', '>', fields.Datetime.now())
+            ], limit=1)
+
+            task.sprint_id = sprint.id if sprint else False
+
 
 class Technology(models.Model):
     _name = 'manage.technology'
     _description = 'Technology'
 
-    name = fields.Char(string='Name', required=True)
-    description = fields.Char(string='Description')
-    photo = fields.Image(string='Photo')
-    task_ids = fields.Many2many('manage.task', string='Tasks')
+    name = fields.Char(required=True)
+    description = fields.Char()
+    photo = fields.Image()
+    task_ids = fields.Many2many('manage.task')
